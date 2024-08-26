e3de6cd68b63bae7215b37f273e122a2c654d046
pivote1214 <yuta.m.12.1214@gmail.com>
2024-08-26 23:49:56 +0900

    add test

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from src.game.game import Game
+from src.game.player import Player
+from src.agent.random_agent import RandomAgent
+from src.agent.place1_agent import Place1Agent
+from src.agent.select1_agent import Select1Agent
+from src.agent.place1_select1_agent import Place1Select1Agent
+
